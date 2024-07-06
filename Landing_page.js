0473a2d3aac57c8cc6709a7ee6b42db67a58cf0a
Akif Zia <160115336+AkifZia9184@users.noreply.github.com>
2024-07-07 02:27:37 +0500

    added navigation menu
    Navigation menu that changes color and font when hover and scrolled

---
+++ Landing_page.js
@@ -0,0 +1,46 @@
+function dropdown_home(){
+    // alert("hello");
+    document.getElementById("home").style.display="inline-block";
+}
+function closee(){
+    document.getElementById("network").style.display="none";
+    document.getElementById("jobs").style.display="none";
+    document.getElementById("message").style.display="none";
+    document.getElementById("noti").style.display="none";
+}
+
+
+
+function dropdown_network(){
+    // alert("hello");
+    document.getElementById("network").style.display="inline-block";
+}
+// function close_network(){
+// }
+
+
+
+function dropdown_jobs(){
+    // alert("hello");
+    document.getElementById("jobs").style.display="inline-block";
+}
+// function close_jobs(){
+// }
+
+
+
+function dropdown_message(){
+    // alert("hello");
+    document.getElementById("message").style.display="inline-block";
+}
+// function close_message(){
+// }
+
+
+
+function dropdown_noti(){
+    // alert("hello");
+    document.getElementById("noti").style.display="block";
+}
+// function close_home(){
+// }
